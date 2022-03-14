bc69042d966ccc1b6c540565fee52c0c9ae9a660
rm5973 <masurnaikrakshit@gmail.com>
2022-03-14 18:59:45 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import React from "react";
 import Abt from "./compo/abt";
 import Navbar from "./compo/navbar";
 import Projects from "./compo/proj";
-
+import Contact from "./compo/constact";
 function App() {
   return (<div> 
     <div id="home">   <Navbar />
  
 </div>
 <div className=" bg-black   h-screen " id="about" ><Abt /></div>
-  <div className="bg-black h-screen" id="projects"> 
-  <Projects />
-  </div>
- 
+<div className="bg-black h-screen" id="projects"><Projects /></div>
+<div className="bg-black h-screen" id="contactus"> <Contact /> </div> 
  </div>
 
  
---
+++ src/compo/constact.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from 'react'
+import PhoneContactimg1 from '../IMAGE/Contactus/PhoneContactimg1.jpg'
+function useWindowSize(){
+    const [size,setSize]=useState([window.innerHeight,window.innerwidth])
+    useEffect(()=>{
+        const handelResize=()=>{
+            setSize([window.innerHeight,window.innerwidth])
+        };
+        window.addEventListener("resize",handelResize);
+
+    },[])
+    return size
+}
+export default function Contact() {
+    const [height,width]=useWindowSize();
+    return (<div class="" >
+        <h1 class="text-white pt-[10px] pl-[40px] text-[50px]">Contact With Us</h1>
+        <div class=" h-3/4 flex">
+            <div class="ml-[100px] w-[400px] h-[500px] ">
+            <img src={PhoneContactimg1} alt="no img found" class="h-[100%] w-[100%]  object-cover" />
+            </div>
+
+
+        <div class="">
+            <div class=" mt-[125px] w-[215px] ml-[20.75rem]">
+
+                <form action="onclick" method="post" class="">
+                    <div class="pt-2  w-[50px] "><input type="text" name="name" placeholder='NAME' class="w-[215px]" /></div>
+                    <div class="pt-2  "> <input type="email" name="emailid" placeholder='EMAIL' class="w-[215px]" /></div>
+                    <div class="pt-2  "><input type="text" name="message" placeholder='MESSAGE' class="w-[215px] h-[80px] text-center" /></div>
+                </form>
+
+
+            </div>
+
+
+
+
+        </div></div>
+        
+    </div>)
+}

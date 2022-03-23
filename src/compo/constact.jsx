fb81c91c7bd30d59f592bd125bec0f25ef382032
rm5973 <masurnaikrakshit@gmail.com>
2022-03-24 00:30:59 +0530

    backend update

--- src/compo/constact.jsx
+++ src/compo/constact.jsx
@@ -1,34 +1,64 @@
 import { useState, useEffect } from "react";
-import PhoneContactimg1 from "../IMAGE/Contactus/PhoneContactimg1.jpg";
+import pone from "../IMAGE/Contactus/pone.svg";
 
 
 
 export default function Contact() {
+
+  const[name,setName]=useState("");
+  const[email,setEmail]=useState("");
+  const[message,setMessage]=useState("");
+  const handleOnSubmit=async (e)=>{
+    
+    let result=await fetch(
+      'http://localhost:8000/register', {
+        method: "post",
+        body: JSON.stringify({ name, email , message}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    })
+    
+    let resdata=await result.json()
+    console.warn(resdata);
+   
+    if(resdata){
+       alert("data entered sucessfully")
+      setName("")
+      setEmail("")
+      setMessage("")
+    
+    
+    }
+  }
 
   return (
     <div class="">
       <h1 class="text-white pt-[10px] pl-[40px] text-[50px]">
         Contact With Us
       </h1>
-      <div class=" ">
+      <div class=" pb-4">
         <div class=" sm:w-2/3 inline-block">
           <img
-            src={PhoneContactimg1}
+            src={pone}
             alt="no img found"
             class=" w-[100%]  "
           />
+          
         </div>
 
         <div class="sm:inline-block sm:w-1/3  align-top p-3">
           <div class=" my-[15vw]">
             <div class=" ">
-              <form action=" " method="POST" >
+              <form   >
 
                 <input
                   type="text"
                   name="name"
                   placeholder="NAME"
                   class=" mt-2 w-3/4 mx-auto block"
+                  value={name} onChange={(e) => setName(e.target.value)}
                 />
 
 
@@ -38,6 +68,7 @@ export default function Contact() {
                   name="emailid"
                   placeholder="EMAIL"
                   class="  w-3/4 mt-2 mx-auto block"
+                  value={email} onChange={(e) => setEmail(e.target.value)}
                 />
 
 
@@ -46,6 +77,7 @@ export default function Contact() {
                   name="message"
                   placeholder="MESSAGE"
                   class=" text-center w-3/4 m-2 mx-auto block"
+                  value={message} onChange={(e) => setMessage(e.target.value)}
                 />
                 <div class="text-white mx-auto w-3/4"><input type="checkbox" class=" mx-auto " /> terms and conditions</div>
 
@@ -54,7 +86,7 @@ export default function Contact() {
                 <button
                   type="submit"
                   class=" mx-auto text-center border-2 bg-white text-black block mt-2"
-                  
+                  onClick={handleOnSubmit}
                 >
                   submit
                 </button>
--- src/compo/subscribe.jsx
+++ src/compo/subscribe.jsx
@@ -1,5 +1,25 @@
-export default function Subscribe() {
+import { useState } from "react"
 
+export default function Subscribe() {
+ const[email,setEmail]=useState("")
+ const handelOnclick=async (e)=>{
+     let result=await fetch(
+        'http://localhost:8000/subscribe',{
+            method:"post",
+            body:JSON.stringify({email}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        let resp= await result.json()
+        console.warn(resp)
+        if(resp){
+            alert("u have been subscribed")
+            setEmail("")
+            
+        }
+
+ }
     return (
        
     <div class="">
@@ -8,9 +28,9 @@ export default function Subscribe() {
             <p class="text-center text-[#7d8285]">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
         </div>
         <div class="flex flex-col justify-center items-center ">
-          <form class=" mb-2" method="post">
-              <div class="inline-block"><input class="mt-2 mx-auto block h-[30px] " type="email" name="email" placeholder="Your Email" /></div>
-            <div class="inline-block"> <button class="text-center  bg-blue-500 h-[30px] text-white rounded  ml-2 mx-auto block" type="submit">Subscribe </button></div>
+          <form class=" mb-2" >
+              <div class="inline-block"><input class="mt-2 mx-auto block h-[30px] " type="email" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Your Email" /></div>
+            <div class="inline-block"> <button class="text-center  bg-blue-500 h-[30px] text-white rounded  ml-2 mx-auto block" type="submit" onClick={handelOnclick}>Subscribe </button></div>
            
         </form>  
         </div>

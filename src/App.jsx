ec725bb655f35068f70c1507784b25bbf31d9a65
vansh619 <vanshchaudhary77@gmail.com>
2022-03-30 14:06:28 +0530

    💄  adding carousel components

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Contact from "./compo/constact";
 
 import Footer from "./compo/footer";
 import Subscribe from "./compo/subscribe";
+import CarouselComponent from "./compo/carouselComponent"
 function App() {
   return (<div> 
     <div id="home">   <Navbar /></div>
 <div className="bg-black " id="about" ><Abt /></div>
-
+<div className="bg-black " id="about" ><CarouselComponent /></div>
 <div className="bg-black " id="projects"><Projects /></div>
 <div className="bg-black " id="contactus"> <Contact /> </div> 
  <div className="bg-black" id="subscription"> <Subscribe/></div>
